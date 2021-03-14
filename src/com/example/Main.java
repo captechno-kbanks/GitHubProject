13375ceb75fdf88658b4567d61a24b4a771597d1
captechno-kbanks <kbanks@captechno.com>
2021-03-14 06:26:19 -0500

    These are my 2nd changes.

---
+++ src/com/example/Main.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from GitHub!");
+        var person = new Person();
+        System.out.println(person);
+
+    }
+}
